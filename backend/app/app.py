25ffad0a89d94ab25662f0411555ff827162905c
mlgarchery <martial.garchery@gmail.com>
2022-10-22 00:45:06 +0200

    add backend base config

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+STATE = {}
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/code")
+async def code(json: dict):
+    STATE[json['key']] = eval(json['code'])
+    return {"STATE": STATE}
