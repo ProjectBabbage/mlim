70eb1a102b3ce5f453d30381bfca37d1d11f5940
Elias Sebbar <demibsel@protonmail.com>
2022-10-22 11:49:14 +0200

    push things that dont work :)

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,11 +2,28 @@ import Head from 'next/head'
 import Image from 'next/image'
 import REPL from '../components/repl'
 import styles from '../styles/Home.module.css'
-import MathJax from 'mathjax-full'
+
+const {mathjax} = require('mathjax-full/js/mathjax.js');
+const {TeX} = require('mathjax-full/js/input/tex.js');
+const {CHTML} = require('mathjax-full/js/output/chtml.js');
+const {liteAdaptor} = require('mathjax-full/js/adaptors/liteAdaptor.js');
+const {RegisterHTMLHandler} = require('mathjax-full/js/handlers/html.js');
+const {AssistiveMmlHandler} = require('mathjax-full/js/a11y/assistive-mml.js');
+
+const {AllPackages} = require('mathjax-full/js/input/tex/AllPackages.js');
 
 export default function Home() {
 
-  MathJax.tex2svg('\\frac{1}{x^2-1}', {display: true})
+  const adaptor = liteAdaptor();
+  const handler = RegisterHTMLHandler(adaptor);
+
+  const tex = new TeX();
+  const chtml = new CHTML();
+  const html = mathjax.document('\[ \int_{a}^{b} x^2 \,dx \]', {InputJax: tex, OutputJax: chtml});
+
+  //console.log(adaptor.textContent(chtml.styleSheet(html)))
+  console.log(chtml)
+  //console.log(html.document.body)
 
   return (
     <div className={styles.container}>
