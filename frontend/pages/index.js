fee94b87326098c0cdb3de839efdb68557803cec
Elias Sebbar <demibsel@protonmail.com>
2022-10-22 12:38:17 +0200

    fix katex & repl

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,28 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
-const {mathjax} = require('mathjax-full/js/mathjax.js');
-const {TeX} = require('mathjax-full/js/input/tex.js');
-const {CHTML} = require('mathjax-full/js/output/chtml.js');
-const {liteAdaptor} = require('mathjax-full/js/adaptors/liteAdaptor.js');
-const {RegisterHTMLHandler} = require('mathjax-full/js/handlers/html.js');
-const {AssistiveMmlHandler} = require('mathjax-full/js/a11y/assistive-mml.js');
-
-const {AllPackages} = require('mathjax-full/js/input/tex/AllPackages.js');
+import REPL from '../components/repl';
+import Katex from './katex';
+import { useState } from 'react';
 
 export default function Home() {
-
-  const adaptor = liteAdaptor();
-  const handler = RegisterHTMLHandler(adaptor);
-
-  const tex = new TeX();
-  const chtml = new CHTML();
-  const html = mathjax.document('\[ \int_{a}^{b} x^2 \,dx \]', {InputJax: tex, OutputJax: chtml});
-
-  //console.log(adaptor.textContent(chtml.styleSheet(html)))
-  console.log(chtml)
-  //console.log(html.document.body)
-
+  const [currentInput, setCurrentInput] = useState("")
   return (
     <div className={styles.container}>
       <Head>
@@ -32,7 +15,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-
+        <Katex/>
         <div className="text-orange-500">
           Put a REPL here ⬇
         </div>
