2f5ac8a301ed6f6a55173b358690d36f345efe92
Julien Raspaud <julien.raspaud@illuin.tech>
2022-10-22 18:22:03 +0200

    Add possibility to add lines

--- frontendts/components/Cell.tsx
+++ frontendts/components/Cell.tsx
@@ -1,15 +1,29 @@
 import {Cell} from "../models/cells";
 import Line from "./Line";
+import {useState} from "react";
+import {LineFactory} from "../services/line-factory";
 
 interface CellProps {
+    id: number;
     cell: Cell;
 }
 
 export default function CellComponent(props: CellProps) {
+    const [lines, setLines] = useState(props.cell.lines);
+
+    function addLine(): void {
+        const newLine = LineFactory.createCodeLine();
+        setLines([...lines, newLine]);
+    }
+
     return (
         <div>
-            <h2>Cell</h2>
-            { props.cell.lines.map((line, i) => <Line line={line} key={i}></Line>) }
+            <h2>Cell #{props.id}</h2>
+            { lines.map((line, i) => <Line line={line} key={i}></Line>) }
+            <div className="action-container">
+                <div className="flex-grow"></div>
+                <button className="icon-button" type="button" onClick={addLine}>+</button>
+            </div>
         </div>
     )
 }
--- frontendts/components/Cells.tsx
+++ frontendts/components/Cells.tsx
@@ -15,8 +15,8 @@ export default function Cells() {
 
     return (
         <div className="container">
-            <h1>My Cells</h1>
-            { cells.map((cell, i) => <div id={`cell${i}`} key={i}><CellComponent cell={cell}></CellComponent></div>) }
+            <h1>MLIM</h1>
+            { cells.map((cell, i) => <div id={`cell${i}`} key={i}><CellComponent cell={cell} id={i}></CellComponent></div>) }
             <div className="action-container">
                 <div className="flex-grow"></div>
                 <div>
