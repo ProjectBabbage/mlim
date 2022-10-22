09a1e97270452f8ed326d38052210e94669859e5
Quentin Garchery <garchery.quentin@gmail.com>
2022-10-22 19:09:28 +0200

    feat: sum as a program

--- backend/model.py
+++ backend/model.py
@@ -11,6 +11,23 @@ class Prog:
         return self.prog()
 
 
+class Sum(Prog):
+    def __init__(self, var: str, init: Prog, end: Prog, body: Prog):
+        self.var = var
+        self.init = init
+        self.end = end
+        self.body = body
+
+    def __call__(self):
+        v_init = int(self.init())
+        v_end = int(self.end())
+        s = 0
+        for k in range(v_init, v_end + 1):
+            context[self.var] = k
+            s += self.body()
+        return s
+
+
 class Value(Prog):
     def __init__(self, value: int):
         self.value = value
@@ -20,11 +37,11 @@ class Value(Prog):
 
 
 class Var(Prog):
-    def __init__(self, value: str):
-        self.value = value
+    def __init__(self, var: str):
+        self.var = var
 
     def __call__(self):
-        return context[self.value]
+        return context[self.var]
 
 
 class BinOp(Prog):
