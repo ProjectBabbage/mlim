bbadaa7e11f13e2c867b4e919fa965ca7c674c32
Julien Raspaud <julien.raspaud@illuin.tech>
2022-10-22 18:15:08 +0200

    Add cells and line style

--- frontendts/components/Cells.tsx
+++ frontendts/components/Cells.tsx
@@ -10,14 +10,19 @@ export default function Cells() {
     function addCell(): void {
         const newCell: Cell = CellFactory.createCell();
         newCell.lines.push(LineFactory.createCodeLine());
-        setCells([...cells, newCell])
+        setCells([...cells, newCell]);
     }
 
     return (
-        <div>
-            <h2>My Cells</h2>
-            <button type="button" onClick={addCell}>Nouvelle cellule</button>
-            { cells.map((cell, i) => <CellComponent cell={cell} key={i}></CellComponent>) }
+        <div className="container">
+            <h1>My Cells</h1>
+            { cells.map((cell, i) => <div id={`cell${i}`} key={i}><CellComponent cell={cell}></CellComponent></div>) }
+            <div className="action-container">
+                <div className="flex-grow"></div>
+                <div>
+                    <button type="button" onClick={addCell}>Nouvelle cellule</button>
+                </div>
+            </div>
         </div>
     )
 }
