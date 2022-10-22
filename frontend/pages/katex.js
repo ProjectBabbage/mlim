0cc9e3f8bda0280174f73953132cc0b020f4a278
mlgarchery <martial.garchery@gmail.com>
2022-10-22 12:26:42 +0200

    katex

---
+++ frontend/pages/katex.js
@@ -0,0 +1,10 @@
+import {BlockMath} from 'react-katex';
+
+export default function Katex() {
+    return (
+      <div>
+        <BlockMath math="\int_0^\infty x^2 dx"/>
+      </div>
+    )
+  }
+  
