43cbde33cbcc48395426bf103a7eed32aec67f9e
Gabriel <gabriel.astieres@gmail.com>
2022-10-22 23:19:36 +0200

    ✨ gradient descent

---
+++ backend/gradient_descent.py
@@ -0,0 +1,70 @@
+MAX_STEP = 25
+EPSILON = 0.001
+
+
+def f(x):
+    return (x + 10) ** 2
+
+
+def gradient_descent(
+    fct_to_descent,
+    last_position,
+    last_position_value,
+    step_size,
+    step_iteration,
+    seen_positions,
+    seen_values,
+):
+
+    left = last_position - step_size
+    left_value = fct_to_descent(left)
+    right = last_position + step_size
+    right_value = fct_to_descent(right)
+    if last_position_value is not None:
+        if (
+            last_position_value < left
+            and last_position_value < right
+            or last_position_value - min(left_value, right_value) < EPSILON
+            or step_iteration > MAX_STEP
+        ):
+            return seen_positions, seen_values
+
+    if left_value == right_value:
+        gradient_descent(
+            fct_to_descent,
+            last_position,
+            last_position_value,
+            step_size / 4,
+            step_iteration + 1,
+        )
+
+    if left_value < right_value:
+        seen_values.append(left_value)
+        seen_positions.append(left)
+
+        return gradient_descent(
+            fct_to_descent,
+            left,
+            left_value,
+            step_size / 2,
+            step_iteration + 1,
+            seen_positions,
+            seen_values,
+        )
+
+    if right_value < left_value:
+        seen_values.append(right_value)
+        seen_positions.append(right)
+
+        return gradient_descent(
+            fct_to_descent,
+            right,
+            right_value,
+            step_size / 2,
+            step_iteration + 1,
+            seen_positions.append(right),
+            seen_values.append(right_value),
+        )
+
+
+print(gradient_descent(f, 0, None, 5, 0, [0], [f(0)]))
