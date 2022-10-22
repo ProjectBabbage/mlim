3e029f0bc76f549a4cf5e966dadf49847b8c9492
Elias Sebbar <demibsel@protonmail.com>
2022-10-22 23:16:13 +0200

    move line content management into cell

--- frontendts/components/Cell.tsx
+++ frontendts/components/Cell.tsx
@@ -1,9 +1,10 @@
-import { Cell } from "../models/cells";
+import { AbstractLine, Cell } from "../models/cells";
 import Line from "./Line";
 import { useState } from "react";
 import { LineFactory } from "../services/line-factory";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus, faTimes} from "@fortawesome/free-solid-svg-icons";
 
 interface CellProps {
     id: number;
@@ -14,16 +15,48 @@ interface CellProps {
 export default function CellComponent({id, cell, callApi}: CellProps) {
     const [result, setResult] = useState('');
     const [lines, setLines] = useState(cell.lines);
+    const [editorEnabled, setEditorEnabled] = useState(true);
+    const [currentLineIndex, setCurrentLineIndex] = useState<number | null>(null);
+
+    function toggleEditor(): void {
+        setEditorEnabled(!editorEnabled)
+    }
 
     const addLine = () => {
         const newLine = LineFactory.createCodeLine();
         setLines([...lines, newLine]);
     }
 
-    const lineUpdate = (i: number, content: string) => {
-        let l = lines;
-        l[i].content = content;
+    const lineUpdate = (lineIndex: number, content: string) => {
+        console.log(`line update ${lineIndex} ${content}`)
+        /*
+            We have to create a new instance of the list 
+            to ensure triggering functions useEffect 
+        */
+        let l = []; 
+        for(let i = 0 ; i<lines.length ; i++){
+            if(i === lineIndex){
+                let newLine = null
+                switch(lines[i].type){
+                    case 'code':
+                        newLine = LineFactory.createCodeLine()
+                        break;
+                    case 'text':
+                        newLine = LineFactory.createTextLine()
+                        break;
+                }
+                newLine.content = content
+                l.push(newLine)
+            }
+            else{
+                l.push(lines[i])
+            }
+        }
         setLines(l);
+    }
+
+    const lineFocused = (line: AbstractLine, index: number) => {
+        setCurrentLineIndex(index)
     }
     
     const execLines = async () => {
@@ -35,6 +68,22 @@ export default function CellComponent({id, cell, callApi}: CellProps) {
         setResult(results.join('\n')); 
     }
 
+    function addSum(): void {
+        if(currentLineIndex !== null)
+            lineUpdate(currentLineIndex, `${lines[currentLineIndex].content}\\sum`)
+
+    }
+
+    function addPlus(): void {
+        if(currentLineIndex !== null)
+            lineUpdate(currentLineIndex, `${lines[currentLineIndex].content}+`)
+    }
+
+    function addTimes(): void {
+        if(currentLineIndex !== null)
+            lineUpdate(currentLineIndex, `${lines[currentLineIndex].content}\\times`)
+    }
+
     return (
         <div className="cell-component">
             <div className="cell-header">
@@ -43,7 +92,13 @@ export default function CellComponent({id, cell, callApi}: CellProps) {
                     <FontAwesomeIcon className="play-icon" icon={faPlay} />
                 </div>
             </div>
-            { lines.map((line, i) => <div className="sick-fade-in" key={i}><Line line={line} lineNumber={i} lineUpdate={lineUpdate}/></div>) }
+            <div className="line-actions">
+                <button className="line-action" onClick={addSum}>Σ</button>
+                <button className="line-action" onClick={addPlus}><FontAwesomeIcon icon={faPlus} /></button>
+                <button className="line-action" onClick={addTimes}><FontAwesomeIcon icon={faTimes} /></button>
+                <div className="flex-grow cursor-pointer" onClick={() => toggleEditor()}></div>
+            </div>
+            { lines.map((line, i) => <div className="sick-fade-in" key={`${id}${i}`}><Line line={line} lineNumber={i} lineUpdate={lineUpdate} lineFocused={lineFocused}/></div>) }
             <div className="action-container">
                 <div className="flex-grow"></div>
                 <button className="icon-button" type="button" onClick={() => addLine()}>+</button>
