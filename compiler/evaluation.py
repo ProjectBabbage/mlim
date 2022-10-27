d51fcd49f4addef35e3878059aab2a7d98aaf30f
Quentin Garchery <garchery.quentin@gmail.com>
2022-10-27 23:58:31 +0200

    refactor: fix and unify style of parser

--- compiler/evaluation.py
+++ compiler/evaluation.py
@@ -1,5 +1,5 @@
 from compiler import parser
-from compiler.model import Value, Matrix
+from compiler.model import Value, Matrix, State
 
 
 def evaluation(input_tex):
@@ -22,8 +22,8 @@ if __name__ == "__main__":
     import sys
 
     input_tex = open(sys.argv[1]).read()
-    st = {
+    State.store = {
         "A": Matrix([[Value(6.0), Value(7.0)], [Value(8.0), Value(9.0)]]),
         "i": Value(1.0),
     }
-    print(evaluation(st, input_tex))
+    print(evaluation(input_tex))
--- compiler/parser.py
+++ compiler/parser.py
@@ -9,88 +9,74 @@ precedence = (
 )
 
 
-def p_program_assign(p):
-    "program : VAR SEMICOL func"
-    model.State.store[p[1]] = p[3]
-    p[0] = p[3]
+def p_program(p):
+    """program : VAR SEMICOL func
+    | func"""
+    if len(p) == 4:
+        model.State.store[p[1]] = p[3]
+        p[0] = p[3]
+    else:
+        p[0] = p[1]
 
 
-def p_program_prog(p):
-    "program : func"
-    p[0] = p[1]
+def p_func(p):
+    """func : VAR MAPSTO prog
+    | prog"""
+    if len(p) == 4:
+        p[0] = model.Function(p[1], p[3])
+    else:
+        p[0] = p[1]
 
 
-def p_func_function(p):
-    "func : VAR MAPSTO prog"
-    p[0] = model.Function(p[1], p[3])
+def p_prog(p):
+    """prog : SUM UNDERS LBRACK VAR EQUALS prog RBRACK CARET LBRACK prog RBRACK mulprog
+    | prog ADDOP prog
+    | mulprog"""
+    if len(p) == 13:
+        p[0] = model.Sum(p[4], p[6], p[10], p[12])
+    elif len(p) == 4:
+        p[0] = model.BinOp(p[1], p[2], p[3])
+    else:
+        p[0] = p[1]
 
 
-def p_func_prog(p):
-    "func : prog"
-    p[0] = p[1]
+def p_mulprog(p):
+    """mulprog : PRODUCT UNDERS LBRACK VAR EQUALS prog RBRACK CARET LBRACK prog RBRACK literal
+    | mulprog MULOP mulprog
+    | literal"""
+    if len(p) == 13:
+        p[0] = model.Product(p[4], p[6], p[10], p[12])
+    elif len(p) == 4:
+        p[0] = model.BinOp(p[1], p[2], p[3])
+    else:
+        p[0] = p[1]
 
 
-def p_prog_addop(p):
-    "prog : prog ADDOP prog"
-    p[0] = model.BinOp(p[1], p[2], p[3])
+def p_literal(p):
+    """literal : BEGIN LBRACK MULTILINE RBRACK multiline END LBRACK MULTILINE RBRACK
+    | VAR UNDERS LBRACK prog COMMA prog RBRACK
+    | VAR LPAREN prog RPAREN
+    | LPAREN prog RPAREN
+    | NABLA VAR
+    | VAR
+    | NUMBER"""
+    if len(p) == 10:
+        p[0] = model.Matrix(p[5])
+    elif len(p) == 7:
+        p[0] = model.SelectElement(model.State.store[p[1]], p[4], p[6])
+    elif len(p) == 5:
+        p[0] = model.Call(p[1], p[3])
+    elif len(p) == 4:
+        p[0] = p[2]
+    elif len(p) == 3:
+        p[0] = model.GradientDescent(p[2])
+    elif len(p) == 2 and isinstance(p[1], float):
+        p[0] = model.Value(p[1])
+    elif len(p) == 2 and isinstance(p[1], str):
+        p[0] = model.Var(p[1])
 
 
-def p_prog_sum(p):
-    "prog : SUM UNDERS LBRACK VAR EQUALS prog RBRACK CARET LBRACK prog RBRACK mulprog"
-    p[0] = model.Sum(p[4], p[6], p[10], p[12])
-
-
-def p_prog_mulprog(p):
-    "prog : mulprog"
-    p[0] = p[1]
-
-
-def p_mulprog_mulop(p):
-    "mulprog : mulprog MULOP mulprog"
-    p[0] = model.BinOp(p[1], p[2], p[3])
-
-
-def p_mulprog_mul(p):
-    "prog : PRODUCT UNDERS LBRACK VAR EQUALS prog RBRACK CARET LBRACK prog RBRACK mulprog"
-    p[0] = model.Product(p[4], p[6], p[10], p[12])
-
-
-def p_mulprog_lit(p):
-    "mulprog : literal"
-    p[0] = p[1]
-
-
-def p_literal_call(p):
-    "literal : VAR LPAREN prog RPAREN"
-    p[0] = model.Call(p[1], p[3])
-
-
-def p_literal_paren(p):
-    "literal : LPAREN prog RPAREN"
-    p[0] = p[2]
-
-
-def p_literal_nabla(p):
-    "literal : NABLA VAR"
-    p[0] = model.GradientDescent(p[2])
-
-
-def p_literal_var(p):
-    "literal : VAR"
-    p[0] = model.Var(p[1])
-
-
-def p_literal_num(p):
-    "literal : NUMBER"
-    p[0] = model.Value(p[1])
-
-
-def p_expression_multiline(p):
-    "literal : BEGIN LBRACK MULTILINE RBRACK multiline END LBRACK MULTILINE RBRACK"
-    p[0] = model.Matrix(p[5])
-
-
-def p_multiline_line(p):
+def p_multiline(p):
     """multiline : line DOUBLEBS multiline
     | line"""
     if len(p) == 4:
@@ -99,18 +85,13 @@ def p_multiline_line(p):
         p[0] = [p[1]]
 
 
-def p_line_list(p):
+def p_line(p):
     """line : prog AMPER line
     | prog"""
     if len(p) == 4:
         p[0] = [p[1]] + p[3]
     elif len(p) == 2:
         p[0] = [p[1]]
-
-
-def p_literal_cell(p):
-    "literal : VAR UNDERS LBRACK prog COMMA prog RBRACK"
-    p[0] = model.SelectElement(model.State.store[p[1]], p[4], p[6])
 
 
 def p_error(p):
