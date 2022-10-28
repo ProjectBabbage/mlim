e6241e7e3e03846b1553f614f7ce61d31f2ca509
Martial Garchery <martial.garchery@gmail.com>
2022-10-29 01:20:24 +0200

    Start python test CI

--- compiler/tests/test.py
+++ compiler/tests/test.py
@@ -3,8 +3,15 @@ from unittest import TestCase
 from compiler import parser
 
 
-class TestSomething(TestCase):
+class TestBinOps(TestCase):
     def test_addition(self):
         addition_tex = Path("compiler/tests/fixtures/addition.tex").read_text()
         prog = parser.yacc.parse(addition_tex)
         self.assertEqual(prog(), 2)
+
+    def test_simple_product(self):
+        simple_product_tex = Path(
+            "compiler/tests/fixtures/simple_product.tex"
+        ).read_text()
+        prog = parser.yacc.parse(simple_product_tex)
+        self.assertEqual(prog(), 120)
